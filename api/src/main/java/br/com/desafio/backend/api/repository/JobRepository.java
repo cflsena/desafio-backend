c5eacbb1c51d965df29a01cd736ea0329bb721e2
Cleiton Sena <cflsena2@gmail.com>
2020-12-06 13:54:47 -0300

    Removendo pacote não utilizado

--- api/src/main/java/br/com/desafio/backend/api/repository/JobRepository.java
+++ api/src/main/java/br/com/desafio/backend/api/repository/JobRepository.java
@@ -1,7 +1,5 @@
 package br.com.desafio.backend.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.desafio.backend.api.entity.JobEntity;
