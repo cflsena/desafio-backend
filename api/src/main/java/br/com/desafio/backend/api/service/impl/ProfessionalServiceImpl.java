006707381c94ba4cf07b7d03207247c8abcace9d
Cleiton Sena <cflsena2@gmail.com>
2020-12-06 13:44:52 -0300

    Adicionando anotação @service

--- api/src/main/java/br/com/desafio/backend/api/service/impl/ProfessionalServiceImpl.java
+++ api/src/main/java/br/com/desafio/backend/api/service/impl/ProfessionalServiceImpl.java
@@ -2,12 +2,14 @@ package br.com.desafio.backend.api.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import br.com.desafio.backend.api.entity.ProfessionalEntity;
 import br.com.desafio.backend.api.repository.ProfessionalRepository;
 import br.com.desafio.backend.api.service.ProfessionalService;
 import br.com.desafio.backend.api.service.common.GenericServiceAbstract;
 
+@Service
 public class ProfessionalServiceImpl extends GenericServiceAbstract<ProfessionalEntity, Long>
 		implements ProfessionalService {
 
