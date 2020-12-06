25eb064f6dadf124884ad2f42983968141060a7f
Cleiton Sena <cflsena2@gmail.com>
2020-12-06 12:32:54 -0300

    Adicionando arquivo sql com inserts padroes

---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Professional (id, name, email, cell_phone) VALUES (1, 'Cleiton Sena', 'teste@teste.com', '99999999999');
+
+INSERT INTO Category (id, description) VALUES (1, 'Assistência Técnica');
+INSERT INTO Category (id, description) VALUES (2, 'Aulas');
+INSERT INTO Category (id, description) VALUES (3, 'Design e Tecnologia');
+INSERT INTO Category (id, description) VALUES (4, 'Saude');
+INSERT INTO Category (id, description) VALUES (5, 'Reformas');
