318c280f872202f0285d3d7c54e2cd359e6253a7
Cleiton Sena <cflsena2@gmail.com>
2020-12-07 04:56:25 -0300

    Ajuste

--- api/src/main/java/br/com/desafio/backend/api/service/impl/ProfessionalServiceImpl.java
+++ api/src/main/java/br/com/desafio/backend/api/service/impl/ProfessionalServiceImpl.java
@@ -1,13 +1,12 @@
 package br.com.desafio.backend.api.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
 import br.com.desafio.backend.api.entity.ProfessionalEntity;
 import br.com.desafio.backend.api.repository.ProfessionalRepository;
 import br.com.desafio.backend.api.service.ProfessionalService;
 import br.com.desafio.backend.api.service.common.GenericServiceAbstract;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProfessionalServiceImpl extends GenericServiceAbstract<ProfessionalEntity, Long>
@@ -18,7 +17,7 @@ public class ProfessionalServiceImpl extends GenericServiceAbstract<Professional
 
 	@Override
 	public JpaRepository<ProfessionalEntity, Long> getRepository() {
-		return professionalRepository;
+		return this.professionalRepository;
 	}
 
 }
