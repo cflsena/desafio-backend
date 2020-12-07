082be20438b2876d0c1e91c0df0ed85854b9c83e
Cleiton Sena <cflsena2@gmail.com>
2020-12-07 04:45:54 -0300

    Arquivo de configuração

--- api/src/main/java/br/com/desafio/backend/api/config/ApplicationConfig.java
+++ api/src/main/java/br/com/desafio/backend/api/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package br.com.desafio.backend.api.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 public class ApplicationConfig {
