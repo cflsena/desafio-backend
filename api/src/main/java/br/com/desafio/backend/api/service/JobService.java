932e1cfa1023b99b7a5b5c2cf35ca960e8a21088
Cleiton Sena <cflsena2@gmail.com>
2020-12-07 22:27:28 -0300

    Adicionando implementação da atualização do status do serviço

--- api/src/main/java/br/com/desafio/backend/api/service/JobService.java
+++ api/src/main/java/br/com/desafio/backend/api/service/JobService.java
@@ -1,8 +1,11 @@
 package br.com.desafio.backend.api.service;
 
 import br.com.desafio.backend.api.entity.JobEntity;
+import br.com.desafio.backend.api.filter.JobFilter;
+import br.com.desafio.backend.api.repository.custom.paginator.PageCustom;
 import br.com.desafio.backend.api.service.common.interfaces.GenericService;
 
 public interface JobService extends GenericService<JobEntity, Long>{
     void updateJobStatus(Boolean status, Long id);
+    PageCustom findJobByFilter(JobFilter job);
 }
--- api/src/main/java/br/com/desafio/backend/api/service/impl/JobServiceImpl.java
+++ api/src/main/java/br/com/desafio/backend/api/service/impl/JobServiceImpl.java
@@ -1,5 +1,7 @@
 package br.com.desafio.backend.api.service.impl;
 
+import br.com.desafio.backend.api.filter.JobFilter;
+import br.com.desafio.backend.api.repository.custom.paginator.PageCustom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
@@ -8,20 +10,35 @@ import br.com.desafio.backend.api.entity.JobEntity;
 import br.com.desafio.backend.api.repository.JobRepository;
 import br.com.desafio.backend.api.service.JobService;
 import br.com.desafio.backend.api.service.common.GenericServiceAbstract;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl extends GenericServiceAbstract<JobEntity, Long> implements JobService {
 
-	@Autowired
-	private JobRepository jobRepository;
+    @Autowired
+    private JobRepository jobRepository;
 
-	@Override
-	public JpaRepository<JobEntity, Long> getRepository() {
-		return jobRepository;
-	}
+    @Override
+    public JpaRepository<JobEntity, Long> getRepository() {
+        return jobRepository;
+    }
 
-	@Override
-	public void updateJobStatus(Boolean status, Long id) {
+    @Transactional
+    @Override
+    public void updateJobStatus(Boolean status, Long id) {
+        Optional<JobEntity> optAccount = findById(id);
+        validateEntity(optAccount);
+        JobEntity accountToUpdate = optAccount.get();
+        accountToUpdate.setActive(status);
+        getRepository().save(accountToUpdate);
+        getRepository().flush();
+    }
 
-	}
+    @Transactional(readOnly = true)
+    @Override
+    public PageCustom findJobByFilter(JobFilter job) {
+        return null;
+    }
 }
